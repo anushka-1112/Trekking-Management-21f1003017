74fc3983a08d0875665deb04a5cd6ea966cbd4a3
Anushka Krishna <anushkakrishna1101@gmail.com>
2026-06-21 11:19:34 +0530

    Milestone: Database Models Created

--- model.py
+++ model.py
@@ -6,3 +6,8 @@ class User(db.Model):
     user_email = db.Column(db.String(25), primary_key = True)
     user_password = db.Column(db.String(10))
     role = db.Column(db.String(10), default='user')
+
+class Trek(db.Model):
+    trek_id = db.Column(db.Integer(), primary_key=True, autoincrement = True)
+    trek_name = db.Column(db.String())
+    trek_status = db.Column(db.String(), default='active')
