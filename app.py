f2723d5b02509f9ff4ac6da0a1c136c51b33c8dd
Anushka Krishna <anushkakrishna1101@gmail.com>
2026-06-22 11:21:29 +0530

    Basic Functionality

--- app.py
+++ app.py
@@ -1,4 +1,58 @@
 from flask import Flask, render_template, request, redirect
+from model import *
 
-app = Flask()
+app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///trekking_mgmnt.db'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+    admin = User.query.filter_by(role='admin').first()
+    if admin is None:
+        admin=User(user_email='admin@gmail.com',
+                   user_password= 'admin123',
+                   role='admin')
+        db.session.add(admin)
+        db.session.commit()
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    if request.method=='POST':
+        user_email=request.form.get('user_email')
+        user_password = request.form.get('user_password')
+        user = User.query.filter_by(user_email=user_email, user_password=user_password).first()
+        print(user)
+        print(user.role)
+        if user is not None:
+            if user.role=='admin':
+                treks=Trek.query.all()
+                return render_template("admin_dashboard.html", treks=treks)
+            if user.role=='trek_staff':
+                return render_template('trek_staff_dashboard.html')
+            if user.role=='user':
+                return render_template('user_dashboard.html')
+        else:
+            return render_template('login.html')
+
+@app.route('/add_trek', methods=['GET', 'POST'])
+def add_trek():
+    if request.method=='GET':
+        return render_template("add_trek.html")
+    if request.method=='POST':
+        trek_name= request.form.get('trek_name')
+        trek = Trek(trek_name=trek_name)
+        db.session.add(trek)
+        db.session.commit()
+        treks=Trek.query.all()
+        return render_template("admin_dashboard.html", treks=treks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -3,11 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    user_email = db.Column(db.String(25), primary_key = True)
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_email = db.Column(db.String(25))
     user_password = db.Column(db.String(10))
     role = db.Column(db.String(10), default='user')
+    approved = db.Column(db.Boolean(), default=False)
+    blacklisted = db.Column(db.Boolean(), default=False)
 
 class Trek(db.Model):
-    trek_id = db.Column(db.Integer(), primary_key=True, autoincrement = True)
-    trek_name = db.Column(db.String())
+    trek_id = db.Column(db.Integer(), primary_key=True)
+    trek_name = db.Column(db.String(), unique=True)
     trek_status = db.Column(db.String(), default='active')
+    trek_details = db.Column(db.Text(), nullable=True)
+    responsible_trek_staff = db.Column(db.Integer,
+                                       db.ForeignKey('user.user_id'),
+                                       nullable=True)
